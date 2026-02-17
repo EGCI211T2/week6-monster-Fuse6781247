0270fe81299e55d3021b96065e6ad7c1c8ec6237
Fuse6781247 <jaruphat.kos@student.mahidol.edu>
2026-02-17 15:00:38 +0000

    thanos

--- thanos.h
+++ thanos.h
@@ -1,72 +1,30 @@
-#ifndef monster_h
-#define monster_h
-#include <iostream>
-#include <string> // เพิ่มเพื่อให้ใช้ string ได้ไม่ error
-using namespace std;
+#ifndef thanos_h
+#define thanos_h
+#include "monster.h"
 
-class monster{
+class Thanos { 
 private:
-    string name;
-    int hp,potion;
+    int stones;
+    int hp;
 public:
-    void Attack(monster &);
-    void heal();
-    monster(string =" fuse ", int=10,int=1); // ปรับ default HP เป็น 10 ไม่ให้เป็น 0
-    ~monster();
-    monster(int, int);
-    
-    void display();
-    bool operator>(monster &x);
-    
-    void operator+=(int);//Binary
-    void operator+=(monster &);//Binary
-    void operator--();//UNay opearator
-
-    // ส่วนที่เพิ่มเพื่อให้ Thanos สั่งตายได้
-    void die() { hp = 0; }
-};
-
-bool monster::operator>(monster &x){
-    if(hp > x.hp ) return true;
-    else return false;
-}
-
-void monster::operator+=(int a){
-    this ->hp+=a;
-}
-
-void monster::operator+=(monster &x){
-    this->hp+=x.hp;
-    x.hp=0;
-}
-
-void monster::operator--(){
-    if(hp > 0) --hp;
-}
-
-monster::~monster(){
-    cout<<name<< "is gone."<<endl;
-}
-
-monster::monster(int h, int p){
-    hp=h;
-    potion=p;
-    name="Inwza Supa Monsta";
-}
-
-monster::monster(string n, int h, int p ){
-    name = n;
-    hp = h;
-    potion = p;
-    cout<<"Monster"<<name<<"is here"<<endl;
-}
-
-void monster::display(){
-    // เงื่อนไข: ถ้า hp=0 จะไม่ display ตามโจทย์
-    if(hp > 0) {
-        cout<<"Name : "<<name<<endl;
-        cout<<"Hp : "<<hp<<endl;
-        cout<<"Potion : "<<potion<<endl;
+    Thanos(int s=0, int h=1000) : stones(s), hp(h) {}
+    ~Thanos() {
+        cout << "Thanos is gone." << endl;
     }
-}
+
+    void snap_finger(monster m[], int n) {
+        if(stones >= 6) {
+            cout << ">>> SNAP! Half of the monsters disappear... <<<" << endl;
+            for(int i = 0; i < n/2; i++) {
+                m[i].die(); 
+            }
+        } else {
+            cout << "Not enough stones. Current stones: " << stones << endl;
+        }
+    }
+
+    void operator++() { 
+        stones++;
+    }
+}; 
 #endif
